ab4d9d1d92993a736d2de44dab906bd1876f0af9
Jaeung Hyun <jaeung@me.com>
2022-09-20 15:06:01 +0900

    add detectOnFrame

--- lib/flutter_super_resolution.dart
+++ lib/flutter_super_resolution.dart
@@ -36,4 +36,50 @@ class FlutterSuperResolution {
       asynch: asynch,
     );
   }
+
+  static const anchors = [
+    0.57273,
+    0.677385,
+    1.87446,
+    2.06253,
+    3.33843,
+    5.47434,
+    7.88282,
+    3.52778,
+    9.77052,
+    9.16828
+  ];
+
+  Future<List?> detectObjectOnFrame({
+    required List<Uint8List> bytesList,
+    String model = "SSDMobileNet",
+    int imageHeight = 1280,
+    int imageWidth = 720,
+    double imageMean = 127.5,
+    double imageStd = 127.5,
+    double threshold = 0.1,
+    int numResultsPerClass = 1,
+    int rotation = 90, // Android only
+    // Used in YOLO only
+    List anchors = anchors,
+    int blockSize = 32,
+    int numBoxesPerBlock = 5,
+    bool asynch = true,
+  }) {
+    return FlutterSuperResolutionPlatform.instance.detectObjectOnFrame(
+      bytesList: bytesList,
+      model: model,
+      imageHeight: imageHeight,
+      imageWidth: imageWidth,
+      imageMean: imageMean,
+      imageStd: imageStd,
+      threshold: threshold,
+      numResultsPerClass: numResultsPerClass,
+      rotation: rotation,
+      anchors: anchors,
+      blockSize: blockSize,
+      numBoxesPerBlock: numBoxesPerBlock,
+      asynch: asynch,
+    );
+  }
 }
